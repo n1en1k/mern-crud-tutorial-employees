cbc631042f79602880b9dc0e30a45410cb5de2b6
n1en1k <niko.nieminen93@gmail.com>
2023-03-30 21:23:27 +0300

    added client and server

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mern-crud-tutorial-employees
-MERN CRUD tutorial employees
+MERN CRUD tutorial Employees\
+\
+server:\
+docker compose start\
+node index.js\
+\
+client:\
+npm start
---
+++ client/src/components/navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+
+// We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+
+// Here, we display our Navbar
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">
+          <h1>Employee Management</h1>
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/create">
+                <h5 style={{ color: "red" }}>Create Record</h5>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
